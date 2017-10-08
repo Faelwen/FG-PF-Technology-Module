72ff51ec14eb56e10dd0cea6dd17dd9b85c944d8
Faelwen <francis.john.mccarthy@googlemail.com>
2017-10-08 23:41:26 +0200

    Technological gear

--- make_module.py
+++ make_module.py
@@ -121,8 +121,47 @@ def generate_xml_structure(xml_root):
     populate_ai(xml_ref_npcdata)
     populate_cybertech(xml_ref_equipment)
     populate_pharmaceuticals(xml_ref_equipment)
+    populate_tech_gear(xml_ref_equipment)
     populate_traps(xml_ref_npcdata)
     populate_weapon(xml_ref_weapon)
+
+
+def populate_tech_gear(xml_ref_equipment):
+    with open(tech_gear_file, 'r',encoding="utf-8") as csvfile:
+        csvreader = csv.reader(csvfile, delimiter="\t", quotechar='"')
+        row = next(csvreader) #skip header
+        item_number = 0
+        prefix = "techgear"
+
+        for row in csvreader:
+            item_number += 1
+            [i_type, i_subtype, i_name, i_price, i_weight, i_capacity,
+            i_usage, i_description, i_crafting] = row
+            #Ref
+            item_ref = prefix + "{:04d}".format(item_number)
+            xml_ref = etree.SubElement(xml_ref_equipment, item_ref)
+            #Type
+            xml_ref_type = etree.SubElement(xml_ref, "type", type="string")
+            xml_ref_type.text = i_type.strip()
+            #Subtype
+            xml_ref_subtype = etree.SubElement(xml_ref, "subtype", type="string")
+            xml_ref_subtype.text = i_subtype.strip()
+            #Name
+            xml_ref_name = etree.SubElement(xml_ref, "name", type="string")
+            xml_ref_name.text = i_name.strip()
+            #Price
+            xml_ref_cost = etree.SubElement(xml_ref, "cost", type="string")
+            xml_ref_cost.text = i_price.strip()
+            #Weight
+            if i_weight != "" and i_weight != "—":
+                xml_ref_weight = etree.SubElement(xml_ref, "weight", type="number")
+                xml_ref_weight.text = i_weight.strip()
+            #Description
+            xml_ref_desc = etree.SubElement(xml_ref, "description", type="formattedtext")
+            xml_ref_desc.text = "<p><b>Capacity:</b> {0}; <b>Usage:</b> {1}</p>{2}".format(i_capacity, i_usage, i_description).strip().replace('\ufffd','-').replace('\u2014','-').replace('\u2013','-')
+            #Crafting
+            xml_ref_reqs = etree.SubElement(xml_ref, "prerequisites", type="string")
+            xml_ref_reqs.text = i_crafting.strip()
 
 
 def populate_cybertech(xml_ref_equipment):
